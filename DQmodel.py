21f1e3ad73e5a72bd690fb9355cf58d079d9c1c3
Xiaopeng Yi <stevendesora@gmail.com>
2019-05-21 22:19:23 -0700

    Update DQmodel.py

--- DQmodel.py
+++ DQmodel.py
@@ -32,3 +32,23 @@ class AgentAi:
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
+            
+agent = AgentAi(5,5,0.9,0.2)
+            
+for epis in range(episodes):
+
+    state = get_state()
+    state = np.reshape(state, [1, 4])
+
+    for step in range(100):
+        action = agent.act(state)
+        next_state, reward, done = take_action(action)
+        next_state = np.reshape(next_state, [1, 4])
+        if done:
+            reward = -100
+        agent.remember(state, action, reward, next_state, done)
+        state = next_state
+        if done:
+            print("episode: {}/{}, score: {}".format(e, episodes, time_t))
+            break
+    agent.replay(32)
